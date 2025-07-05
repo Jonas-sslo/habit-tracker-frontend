cecbbecdb801b7570ee22dbb17b11c257e977dcc
Guilherme Henrique <henrique.guilhermexv@gmail.com>
2025-07-05 13:08:24 -0300

    refactor: adicionar lógica de montagem ao componente Footer

--- src/app/components/features/landing/Footer.jsx
+++ src/app/components/features/landing/Footer.jsx
@@ -1,11 +1,19 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Instagram, LinkedIn } from '@mui/icons-material';
 import { useTheme } from 'next-themes';
 
 export default function Footer() {
     const { theme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
 
     const isDark = theme === 'dark';
 
